2bbd9e0353b785e03b2df35b9523a2c347036e45
Tyler Cook <tcc@sandpolis.com>
2021-06-15 19:50:43 -0500

    Update buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ dependencies {
 	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.3.2")
 
 	if (project.getParent() == null) {
-		api("com.sandpolis:core.instance:0.1.0")
-		api("com.sandpolis:core.net:0.1.0")
+		api("com.sandpolis:core.instance:0.2.0")
+		api("com.sandpolis:core.net:0.2.0")
 	} else {
 		api(project(":module:com.sandpolis.core.instance"))
 		api(project(":module:com.sandpolis.core.net"))
